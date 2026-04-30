298ec006a6f32da3f55656543ebb1d184b53ffdc
Gabriel Coutinho <156151153+gcoutinhoo@users.noreply.github.com>
2026-04-30 14:15:22 -0300

    Adicionando pdg

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,7 @@ async function getAboutGithub(){
             <div class="about-buttons-data">
             <div class="buttons-container">
                 <a href="${perfil.html_url}" target="_blank" class="botao">Ver GitHub</a>
-                <a href="#" target="_blank" class="botao-outline">Currículo</a>
+                <a href="assets/pdf/CV Gabriel Coutinho.pdf" target="_blank" class="botao-outline">Currículo</a>
             </div>
 
             <div class="data-container">
